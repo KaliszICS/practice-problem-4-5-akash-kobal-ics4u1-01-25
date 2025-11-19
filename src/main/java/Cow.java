e2e83e5386491df759c7d436c76e2fbeddd261bb
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-11-19 17:12:34 -0500

    update javadocs

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,28 +1,69 @@
+/**
+ * Represents a cow with a name, age, and weight.
+ */
 public class Cow {
     String name;
     int age;
     double weight;
 
+    /**
+     * Creates a new Cow with the given name, age, and weight.
+     *
+     * @param name   the cow's name
+     * @param age    the cow's age
+     * @param weight the cow's weight
+     */
     public Cow(String name, int age, double weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
+
+    /**
+     * Returns the cow's name.
+     *
+     * @return the name of the cow
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Returns the cow's age.
+     *
+     * @return the age of the cow
+     */
     public int getAge() {
         return this.age;
     }
+
+    /**
+     * Returns the cow's weight.
+     *
+     * @return the weight of the cow
+     */
     public double getWeight() {
         return this.weight;
     }
 
+    /**
+     * Returns a string representation of the cow as:
+     * "name, age - weight".
+     *
+     * @return a formatted string describing the cow
+     */
     @Override
     public String toString() {
         return this.name + ", " + this.age + " - " + this.weight;
     }
-    
+
+    /**
+     * Determines whether this cow is equal to another object.
+     * Two cows are equal when their name, age, and weight match.
+     *
+     * @param obj the object to compare with
+     * @return true if the cows have the same values; false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -31,7 +72,9 @@ public class Cow {
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
-        Cow other = (Cow)obj;
-        return this.name.equals(other.name) && this.age == other.age && this.weight == other.weight;
+        Cow other = (Cow) obj;
+        return this.name.equals(other.name)
+            && this.age == other.age
+            && this.weight == other.weight;
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,38 +1,78 @@
+/**
+ * Represents a student with a name, age, and student number.
+ */
 public class Student {
     String name;
     int age;
     String studentNumber;
-    
+
+    /**
+     * Creates a new Student with the given name, age, and student number.
+     *
+     * @param name           the student's name
+     * @param age            the student's age
+     * @param studentNumber  the student's unique student number
+     */
     public Student(String name, int age, String studentNumber) {
         this.name = name;
         this.age = age;
         this.studentNumber = studentNumber;
     }
 
+    /**
+     * Returns the student's name.
+     *
+     * @return the name of the student
+     */
     public String getName() {
         return this.name;
     }
-     
+
+    /**
+     * Returns the student's age.
+     *
+     * @return the age of the student
+     */
     public int getAge() {
         return this.age;
     }
+
+    /**
+     * Returns the student's student number.
+     *
+     * @return the student number
+     */
     public String getStudentNumber() {
         return this.studentNumber;
     }
+
+    /**
+     * Returns a string representation of the student in the format:
+     * "name, age - studentNumber".
+     *
+     * @return a formatted string describing the student
+     */
     @Override
     public String toString() {
         return this.name + ", " + this.age + " - " + this.studentNumber;
     }
 
-	@Override
-	public boolean equals(Object obj) { 
-		if (this == obj) {
+    /**
+     * Determines whether this student is equal to another object.
+     * Two students are considered equal when their student numbers match.
+     *
+     * @param obj the object to compare with
+     * @return true if the student numbers are the same; false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
-    	Student other = (Student)obj;
+        Student other = (Student) obj;
         return this.studentNumber.equals(other.studentNumber);
-	}
+    }
 }
